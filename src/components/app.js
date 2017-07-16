35bf5ac55ded08385b82eddc4e85a0dcd8d10d20
nfd <nickfreemandesign>
2017-07-15 17:48:45 -0700

    first steps initializing project with pair

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,16 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+  templateUrl: 'src/templates/app.html',
+  controller: 'ctrl'
+})
+
+
+.controller('ctrl', function($scope) {
+
+  this.searchResults = function() {};
+  this.selectVideo = function() {};
+  this.currentVideo = window.exampleVideoData[0];
+  this.videos = window.exampleVideoData;
+
 });
